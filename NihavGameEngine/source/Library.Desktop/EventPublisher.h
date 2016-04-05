2604fb9bdfefeb175cbbc357a04169b9a4e2596c
Nihav Jain <njain@fiea.ucf.edu>
2016-04-05 13:55:23 -0400

    added comments to event publisher and the subscriber interface

--- NihavGameEngine/source/Library.Desktop/EventPublisher.h
+++ NihavGameEngine/source/Library.Desktop/EventPublisher.h
@@ -6,24 +6,82 @@
 
 namespace Library
 {
+	/**
+	 *	Base class for an Event
+	 *	@inherits RTTI
+	 */
 	class EventPublisher : public RTTI
 	{
 		RTTI_DECLARATIONS(EventPublisher, RTTI);
 	
 	public:
+
+		/**
+		 *	Constructor
+		 *	@param constant reference to the vector of pointers to subscribers of an Event
+		 */
 		explicit EventPublisher(const Vector<IEventSubscriber*>& subscriberList);
+
+		/**
+		 *	Copy Constructor
+		 *	@param constant reference to the EventPublisher to be copied
+		 */
 		EventPublisher(const EventPublisher& rhs);
+
+		/**
+		 *	Move Constructor
+		 *	@param R value reference to the EventPublisher to be moved
+		 */
 		EventPublisher(EventPublisher&& rhs);
 
+		/**
+		 *	default destructor
+		 */
 		virtual ~EventPublisher() = default;
 
+		/**
+		 *	Copy assignment operator
+		 *	@param constant reference to the EventPublisher to be copied
+		 *	@return reference to the EventPublisher
+		 */
 		EventPublisher& operator=(const EventPublisher& rhs);
+
+		/**
+		 *	Move assignment operator
+		 *	@param R value reference to the EventPublisher to be moved
+		 *	@return reference to the EventPublisher
+		 */
 		EventPublisher& operator=(EventPublisher&& rhs);
 
+		/**
+		 *	Sets the time this event was enqueued in the EventQueue
+		 *	@param constant reference to chrono milliseconds of total elapsed game time
+		 *	@param (optional) chrono milliseconds delay for this event to be fired after being enqueued
+		 */
 		void SetTime(const std::chrono::milliseconds& timeEnqueued, std::chrono::milliseconds delay = std::chrono::milliseconds());
+
+		/**
+		 *	Getter for the time this event was enqueued in the EventQueue
+		 *	@return const reference to chrono milliseconds of the elapsed game time when the event was added to the queue
+		 */
 		const std::chrono::milliseconds& TimeEnqueued() const;
+
+		/**
+		 *	Getter for the millisecods to wait after enqueuing to fire the event
+		 *	@return const reference to chrono milliseconds
+		 */
 		const std::chrono::milliseconds& Delay() const;
+
+		/**
+		 *	Checks if this event has expired or not - difference in current time and time enqueued must be more than the specified delay
+		 *	@param const reference to chrono millisecond representing the current total elapsed game time
+		 *	@return true if event has expired, false otherwise
+		 */
 		bool IsExpired(const std::chrono::milliseconds& currentTime) const;
+
+		/**
+		 *	Notifies all the subscribers of this event
+		 */
 		void Deliver();
 
 	private:
--- NihavGameEngine/source/Library.Desktop/IEventSubscriber.h
+++ NihavGameEngine/source/Library.Desktop/IEventSubscriber.h
@@ -4,12 +4,28 @@ namespace Library
 {
 	class EventPublisher;
 
+	/**
+	 *	Abstract interface for an Event Subscriber
+	 */
 	class IEventSubscriber
 	{
 	public:
+
+		/**
+		 *	Default constructor
+		 */
 		IEventSubscriber() = default;
+
+		/**
+		 *	Default destructor
+		 */
 		virtual ~IEventSubscriber() = default;
 
+		/**
+		 *	Called by Event publishers for notifying the subscriber
+		 *	NOTE: pure virtual method - needs to be overriden by the child
+		 *	@param constant reference to the event publisher
+		 */
 		virtual void Notify(const EventPublisher& eventMessage) = 0;
 	};
 
