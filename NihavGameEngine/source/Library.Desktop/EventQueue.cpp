5440d93ed9876dec18a10dc550d329f8aec3d145
Nihav Jain <njain@fiea.ucf.edu>
2016-04-05 13:09:34 -0400

    changed function parameters in EventQueue to take shared_ptr instead of references refactored code in EventQueue

--- NihavGameEngine/source/Library.Desktop/EventQueue.cpp
+++ NihavGameEngine/source/Library.Desktop/EventQueue.cpp
@@ -7,28 +7,21 @@ namespace Library
 		mPublishers(), mExpiredEvents()
 	{}
 
-	void EventQueue::Enqueue(EventPublisher& publisher, const GameTime& gameTime, std::chrono::milliseconds delay)
+	void EventQueue::Enqueue(const std::shared_ptr<EventPublisher>& publisher, const GameTime& gameTime, std::chrono::milliseconds delay)
 	{
-		publisher.SetTime(gameTime.ElapsedGameTime(), delay);
-		mPublishers.PushBack(std::shared_ptr<EventPublisher>(&publisher));
+		publisher->SetTime(gameTime.ElapsedGameTime(), delay);
+		mPublishers.PushBack(publisher);
 	}
 
-	void EventQueue::Send(EventPublisher& publisher)
+	void EventQueue::Send(const std::shared_ptr<EventPublisher>& publisher)
 	{
-		publisher.Deliver();
-		mPublishers.Remove(std::shared_ptr<EventPublisher>(&publisher));
+		publisher->Deliver();
+		mPublishers.Remove(publisher);
 	}
 
 	void EventQueue::Update(const GameTime& gameTime)
 	{
-		for (auto& publisher : mPublishers)
-		{
-			if (publisher->IsExpired(gameTime.ElapsedGameTime()))
-			{
-				publisher->Deliver();
-				mExpiredEvents.PushBack(publisher);
-			}
-		}
+		DeliverExpiredEvents(gameTime);
 
 		for (auto& expiredEvent : mExpiredEvents)
 		{
@@ -40,14 +33,7 @@ namespace Library
 
 	void EventQueue::Clear(const GameTime& gameTime)
 	{
-		for (auto& publisher : mPublishers)
-		{
-			if (publisher->IsExpired(gameTime.ElapsedGameTime()))
-			{
-				publisher->Deliver();
-				mExpiredEvents.PushBack(publisher);
-			}
-		}
+		DeliverExpiredEvents(gameTime);
 		mPublishers.Clear();
 		mExpiredEvents.Clear();
 	}
@@ -62,9 +48,21 @@ namespace Library
 		return mPublishers.Size();
 	}
 
-	void EventQueue::CancelEvent(EventPublisher& publisher)
+	void EventQueue::CancelEvent(const std::shared_ptr<EventPublisher>& publisher)
 	{
-		mPublishers.Remove(std::shared_ptr<EventPublisher>(&publisher));
+		mPublishers.Remove(publisher);
+	}
+
+	void EventQueue::DeliverExpiredEvents(const GameTime& gameTime)
+	{
+		for (auto& publisher : mPublishers)
+		{
+			if (publisher->IsExpired(gameTime.ElapsedGameTime()))
+			{
+				publisher->Deliver();
+				mExpiredEvents.PushBack(publisher);
+			}
+		}
 	}
 
 }
--- NihavGameEngine/source/Library.Desktop/EventQueue.h
+++ NihavGameEngine/source/Library.Desktop/EventQueue.h
@@ -6,25 +6,44 @@
 
 namespace Library
 {
+	/**
+	 *	Class to manage a time based event queue
+	 */
 	class EventQueue final
 	{
 	public:
+		/**
+		 *	default constructor
+		 */
 		EventQueue();
+
+		/**
+		 *	default destructor
+		 */
 		~EventQueue() = default;
 
+		/**
+		 *	disallowed copy constructor
+		 */
 		EventQueue(const EventQueue& rhs) = delete;
+
+		/**
+		 *	disallowed copy constructor
+		 */
 		EventQueue& operator=(const EventQueue& rhs) = delete;
 
-		void Enqueue(EventPublisher& publisher, const GameTime& gameTime, std::chrono::milliseconds delay = std::chrono::milliseconds());
-		void Send(EventPublisher& publisher);
+		void Enqueue(const std::shared_ptr<EventPublisher>& publisher, const GameTime& gameTime, std::chrono::milliseconds delay = std::chrono::milliseconds());
+		void Send(const std::shared_ptr<EventPublisher>& publisher);
 		void Update(const GameTime& gameTime);
 		void Clear(const GameTime& gameTime);
 
 		bool IsEmpty() const;
 		std::uint32_t Size() const;
-		void CancelEvent(EventPublisher& publisher);
+		void CancelEvent(const std::shared_ptr<EventPublisher>& publisher);
 
 	private:
+		void DeliverExpiredEvents(const GameTime& gameTime);
+
 		Vector<std::shared_ptr<EventPublisher>> mPublishers;
 		Vector<std::shared_ptr<EventPublisher>> mExpiredEvents;
 	};
