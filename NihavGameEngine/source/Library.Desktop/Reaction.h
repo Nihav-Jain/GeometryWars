731abbec8d97d64f630260ed1a1cadf6c312bf2e
Nihav Jain <njain@fiea.ucf.edu>
2016-04-08 16:47:50 -0400

    added abstract Reaction - which is an event subscriber and an action list

---
+++ NihavGameEngine/source/Library.Desktop/Reaction.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "ActionList.h"
+#include "IEventSubscriber.h"
+
+namespace Library
+{
+	class Reaction : public ActionList, public IEventSubscriber
+	{
+		RTTI_DECLARATIONS(Reaction, ActionList);
+	public:
+		Reaction() = default;
+		virtual ~Reaction() = default;
+
+		// debatable
+		//virtual void Update(WorldState& worldState) override;
+	};
+
+#define CONCRETE_REACTION_FACTORY(ConcreteReactionType) CONCRETE_FACTORY(ConcreteReactionType, Reaction);
+
+}
+
