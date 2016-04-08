6b6da326b321b761019456e17f00de5d9f7d57af
Nihav Jain <njain@fiea.ucf.edu>
2016-04-08 16:48:43 -0400

    added EventMessageAttributed which will act as the payload for action events and deliver attributes to Reactions

---
+++ NihavGameEngine/source/Library.Desktop/EventMessageAttributed.cpp
@@ -0,0 +1,91 @@
+#include "pch.h"
+#include "World.h"
+#include "EventMessageAttributed.h"
+
+namespace Library
+{
+	RTTI_DEFINITIONS(EventMessageAttributed);
+
+	const std::string EventMessageAttributed::ATTRIBUTE_SUBTYPE = "subtype";
+	const std::string EventMessageAttributed::ATTRIBUTE_ATTRIBUTES = "attributes";
+
+	EventMessageAttributed::EventMessageAttributed() :
+		mSubtype(), mWorldState(nullptr)
+	{
+		Populate();
+	}
+
+	EventMessageAttributed::EventMessageAttributed(const EventMessageAttributed& rhs) :
+		mSubtype(rhs.mSubtype), mWorldState(rhs.mWorldState), Attributed::Attributed(rhs)
+	{
+		(*this)[ATTRIBUTE_SUBTYPE].SetStorage(&mSubtype, 1);
+	}
+
+	EventMessageAttributed::EventMessageAttributed(EventMessageAttributed&& rhs) :
+		mSubtype(std::move(rhs.mSubtype)), mWorldState(rhs.mWorldState), Attributed::Attributed(std::move(rhs))
+	{
+		(*this)[ATTRIBUTE_SUBTYPE].SetStorage(&mSubtype, 1);
+		rhs.mWorldState = nullptr;
+	}
+
+
+	EventMessageAttributed::~EventMessageAttributed()
+	{
+	}
+
+	EventMessageAttributed& EventMessageAttributed::operator=(const EventMessageAttributed& rhs)
+	{
+		if (this != &rhs)
+		{
+			mSubtype = rhs.mSubtype;
+			mWorldState = rhs.mWorldState;
+
+			Attributed::operator=(rhs);
+
+			(*this)[ATTRIBUTE_SUBTYPE].SetStorage(&mSubtype, 1);
+		}
+		return *this;
+	}
+
+	EventMessageAttributed& EventMessageAttributed::operator=(EventMessageAttributed&& rhs)
+	{
+		if (this != &rhs)
+		{
+			mSubtype = std::move(rhs.mSubtype);
+			mWorldState = rhs.mWorldState;
+
+			Attributed::operator=(rhs);
+
+			(*this)[ATTRIBUTE_SUBTYPE].SetStorage(&mSubtype, 1);
+
+			rhs.mWorldState = nullptr;
+		}
+		return *this;
+	}
+
+	void EventMessageAttributed::SetSubtype(const std::string& subtype)
+	{
+		mSubtype = subtype;
+	}
+
+	const std::string& EventMessageAttributed::GetSubtype() const
+	{
+		return mSubtype;
+	}
+
+	void EventMessageAttributed::SetWorldState(WorldState& worldState)
+	{
+		mWorldState = &worldState;
+	}
+
+	WorldState& EventMessageAttributed::GetWorldState() const
+	{
+		assert(mWorldState != nullptr);
+		return *mWorldState;
+	}
+
+	void EventMessageAttributed::DefinePrescribedAttributes()
+	{
+		AddExternalAttribute(ATTRIBUTE_SUBTYPE, 1, &mSubtype);
+	}
+}
---
+++ NihavGameEngine/source/Library.Desktop/EventMessageAttributed.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "Attributed.h"
+
+namespace Library
+{
+	class EventMessageAttributed : public Attributed
+	{
+		RTTI_DECLARATIONS(EventMessageAttributed, Attributed);
+	public:
+		EventMessageAttributed();
+		EventMessageAttributed(const EventMessageAttributed& rhs);
+		EventMessageAttributed(EventMessageAttributed&& rhs);
+
+		virtual ~EventMessageAttributed();
+
+		EventMessageAttributed& operator=(const EventMessageAttributed& rhs);
+		EventMessageAttributed& operator=(EventMessageAttributed&& rhs);
+
+		void SetSubtype(const std::string& subtype);
+		const std::string& GetSubtype() const;
+
+		void SetWorldState(WorldState& world);
+		WorldState& GetWorldState() const;
+
+		static const std::string ATTRIBUTE_SUBTYPE;
+		static const std::string ATTRIBUTE_ATTRIBUTES;
+
+	protected:
+		virtual void DefinePrescribedAttributes() override;
+
+	private:
+		std::string mSubtype;
+		WorldState* mWorldState;
+	};
+}
+
