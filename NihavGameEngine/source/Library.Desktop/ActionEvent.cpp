d66e6425068e5217792ff7b47539a6883c1544a3
Nihav Jain <njain@fiea.ucf.edu>
2016-04-08 16:50:21 -0400

    added ActionEvent which populates the event payload with the requires attributes and queues the event into the event queue

---
+++ NihavGameEngine/source/Library.Desktop/ActionEvent.cpp
@@ -0,0 +1,52 @@
+#include "pch.h"
+#include <memory>
+#include "ActionEvent.h"
+#include "Event.h"
+#include "EventMessageAttributed.h"
+
+namespace Library
+{
+	RTTI_DEFINITIONS(ActionEvent);
+
+	const std::string ActionEvent::ATTRIBUTE_SUBTYPE = "subtype";
+	const std::string ActionEvent::ATTRIBUTE_DELAY = "delay";
+	const std::string ActionEvent::ATTRIBUTE_ATTRIBUTES = "attributes";
+
+	ActionEvent::ActionEvent()
+	{
+		Populate();
+	}
+
+
+	ActionEvent::~ActionEvent()
+	{
+	}
+
+	void ActionEvent::Update(WorldState& worldState)
+	{
+		EventMessageAttributed message;
+		message.SetSubtype((*this)[ATTRIBUTE_SUBTYPE].Get<std::string>());
+		message.SetWorldState(worldState);
+
+		// copy attributes
+		Datum& attributes = (*this)[ATTRIBUTE_ATTRIBUTES];
+		if (attributes.Size() > 0)
+		{
+			Scope* tableOfAttributes = new Scope(*attributes.Get<Scope*>());
+			message.Adopt(EventMessageAttributed::ATTRIBUTE_ATTRIBUTES, *tableOfAttributes);
+		}
+
+		std::int32_t delay = (*this)[ATTRIBUTE_DELAY].Get<std::int32_t>();
+		std::shared_ptr<Event<EventMessageAttributed>> attributedEvent = std::make_shared<Event<EventMessageAttributed>>(message);
+		worldState.world->GetEventQueue().Enqueue(attributedEvent, *worldState.mGameTime, std::chrono::milliseconds(delay));
+
+	}
+
+	void ActionEvent::DefinePrescribedAttributes()
+	{
+		AddInternalAttribute(ATTRIBUTE_SUBTYPE, "", 0);
+		AddInternalAttribute(ATTRIBUTE_DELAY, 0, 1);
+		AddNestedScope(ATTRIBUTE_ATTRIBUTES, 0);
+	}
+
+}
---
+++ NihavGameEngine/source/Library.Desktop/ActionEvent.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Action.h"
+
+namespace Library
+{
+	class ActionEvent : public Action
+	{
+		RTTI_DECLARATIONS(ActionEvent, Action);
+
+	public:
+		ActionEvent();
+		virtual ~ActionEvent();
+
+		ActionEvent(const ActionEvent& rhs) = delete;
+		ActionEvent& operator=(const ActionEvent& rhs) = delete;
+
+		virtual void Update(WorldState& worldState) override;
+
+		static const std::string ATTRIBUTE_SUBTYPE;
+		static const std::string ATTRIBUTE_DELAY;
+		static const std::string ATTRIBUTE_ATTRIBUTES;
+
+	protected:
+		virtual void DefinePrescribedAttributes() override;
+	};
+
+	CONCRETE_ACTION_FACTORY(ActionEvent);
+}
+
