17370d29b040efd06c9bb4197e43ea85503db5ad
John Michael Bell <jmbell@fiea.local>
2016-04-20 14:42:34 -0400

    I have some temp D3D stuff

---
+++ GeometryWars/source/Game.Desktop.DirectX/PolygonRenderer.cpp
@@ -0,0 +1,77 @@
+#include "pch.h"
+#include "PolygonRenderer.h"
+#include "GameException.h"
+#include <vector>
+#include "Utility.h"
+
+using namespace Library;
+PolygonRenderer::PolygonRenderer(ID3D11Device1 & device, ID3D11DeviceContext & context): m_device(&device), m_context(&context), m_pixel_shader(nullptr), m_vertex_shader(nullptr), m_vertex_buffer(m_vertex_buffer)
+{
+
+	////////////////////Move this to util
+
+	WCHAR buffer[MAX_PATH];
+	GetModuleFileName(nullptr, buffer, MAX_PATH);
+	PathRemoveFileSpec(buffer);
+	SetCurrentDirectory(std::wstring(buffer).c_str());
+	///////////////////
+	// Load a compiled vertex shader
+	std::vector<char> compiledVertexShader;
+	Utility::LoadBinaryFile(L"VertexShader.cso", compiledVertexShader);
+	ThrowIfFailed(device.CreateVertexShader(&compiledVertexShader[0], compiledVertexShader.size(), nullptr, &m_vertex_shader), "ID3D11Device::CreatedVertexShader() failed.");
+
+	// Load a compiled pixel shader
+	std::vector<char> compiledPixelShader;
+	Utility::LoadBinaryFile(L"PixelShader.cso", compiledPixelShader);
+	ThrowIfFailed(device.CreatePixelShader(&compiledPixelShader[0], compiledPixelShader.size(), nullptr, &m_pixel_shader), "ID3D11Device::CreatedPixelShader() failed.");
+
+	// Create an input layout
+	D3D11_INPUT_ELEMENT_DESC inputElementDescriptions[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+	};
+
+	ThrowIfFailed(device.CreateInputLayout(inputElementDescriptions, ARRAYSIZE(inputElementDescriptions), &compiledVertexShader[0], compiledVertexShader.size(), &m_input_layout), "ID3D11Device::CreateInputLayout() failed.");
+
+	// Create a vertex buffer
+	VertexPositionColor vertices[] =
+	{
+		VertexPositionColor { XMFLOAT4(-1.0f,-0.5f, 0.5f, 1.f), XMFLOAT4(1.0f, 0, 0, 0)},
+		VertexPositionColor { XMFLOAT4(-0.5f, 0.5f, 0.5f, 1.f), XMFLOAT4(0, 1.0f, 0, 0)},
+		VertexPositionColor { XMFLOAT4(0.0f,-0.5f, 0.5f, 1.f), XMFLOAT4(0, 0, 1.0f, 0)},
+		VertexPositionColor { XMFLOAT4(0.0f,-0.5f, 0.5f, 1.f), XMFLOAT4(1.0f, 0, 0, 0)},
+		VertexPositionColor { XMFLOAT4(0.5f, 0.5f, 0.5f, 1.f), XMFLOAT4(0, 1.0f, 0, 0)},
+		VertexPositionColor { XMFLOAT4(1.0f,-0.5f, 0.5f, 1.f), XMFLOAT4(0, 0, 1.0f, 0)}
+	};
+
+	D3D11_BUFFER_DESC vertexBufferDesc;
+	ZeroMemory(&vertexBufferDesc, sizeof(vertexBufferDesc));
+	vertexBufferDesc.ByteWidth = sizeof(VertexPositionColor) * ARRAYSIZE(vertices);
+	vertexBufferDesc.Usage = D3D11_USAGE_IMMUTABLE;
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+
+	D3D11_SUBRESOURCE_DATA vertexSubResourceData;
+	ZeroMemory(&vertexSubResourceData, sizeof(vertexSubResourceData));
+	vertexSubResourceData.pSysMem = vertices;
+	ThrowIfFailed(device.CreateBuffer(&vertexBufferDesc, &vertexSubResourceData, &m_vertex_buffer), "ID3D11Device::CreateBuffer() failed.");
+}
+
+PolygonRenderer::~PolygonRenderer()
+{
+}
+
+void PolygonRenderer::Draw()
+{
+	m_context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	m_context->IASetInputLayout(m_input_layout);
+
+	UINT stride = sizeof(VertexPositionColor);
+	UINT offset = 0;
+	m_context->IASetVertexBuffers(0, 1, &m_vertex_buffer, &stride, &offset);
+
+	m_context->VSSetShader(m_vertex_shader, nullptr, 0);
+	m_context->PSSetShader(m_pixel_shader, nullptr, 0);
+
+	m_context->Draw(6, 0);
+}
---
+++ GeometryWars/source/Game.Desktop.DirectX/PolygonRenderer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+struct ID3D11Device1		 ;
+struct ID3D11DeviceContext;
+struct ID3D11VertexShader ;
+struct ID3D11PixelShader	 ;
+struct ID3D11InputLayout	 ;
+struct ID3D11Buffer		 ;
+
+class PolygonRenderer
+{
+public:
+	PolygonRenderer(ID3D11Device1& device, ID3D11DeviceContext& context);
+	~PolygonRenderer();
+	void Draw();
+
+
+private:
+	struct VertexPositionColor
+	{
+		XMFLOAT4 Position;
+		XMFLOAT4 Color;
+	};
+	ID3D11Device1* m_device;
+	ID3D11DeviceContext* m_context;
+	ID3D11VertexShader* m_vertex_shader;
+	ID3D11PixelShader* m_pixel_shader;
+	ID3D11InputLayout* m_input_layout;
+	ID3D11Buffer* m_vertex_buffer;
+	
+};
+
