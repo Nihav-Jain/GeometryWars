c043b5ea2135dfe5d391608f1e1301347ff49566
Yuhsiang Lu <luki0639@gmail.com>
2016-04-20 15:31:17 -0400

    Add alpha blending

--- GeometryWars/source/Library.Desktop/OpenGLRenderDevice.cpp
+++ GeometryWars/source/Library.Desktop/OpenGLRenderDevice.cpp
@@ -48,6 +48,9 @@ namespace Library {
 		}
 
 		glViewport(0, 0, 800, 600);
+
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	}
 
 	Viewport * OpenGLRenderDevice::CreateViewport()
